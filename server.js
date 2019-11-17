6c8634a6aa9ae3573a1fbc4843f4d7efe7347f4d
Martin <martin@ushakiran-MacBook-Pro.local>
2019-11-17 19:11:09 +0100

    add database ulr

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const db = knex({
 	
     client: 'pg',
     connection: {
-    connectionString : 'process.env.DATABASE_URL',
+    connectionString : process.env.DATABASE_URL,
     ssl: true,
   }
 
