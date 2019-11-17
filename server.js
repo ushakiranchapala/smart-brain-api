6eadba963731ceae4a55a394d2b152e2c6b3f81d
Martin <martin@ushakiran-MacBook-Pro.local>
2019-11-17 19:01:09 +0100

    added heroku db

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const db = knex({
 	
     client: 'pg',
     connection: {
-     host : '127.0.0.1',
+     host : 'postgresql-octagonal-48826',
      user : 'martin',
      password : '',
      database : 'smart-brain'
